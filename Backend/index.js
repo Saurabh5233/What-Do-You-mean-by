192d42459c8b52790ebd9b586454333ace7eec90
Saurabh Shandilya <124551411+Saurabh5233@users.noreply.github.com>
2025-09-13 23:30:08 +0530

    Update index.js , added a url in Cors

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ app.use(passport.session()); // for persistent login sessions
 
 app.use(cors({
     origin: function (origin, callback) {
-        const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000', 'http://localhost:5173'];
+        const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000', 'http://localhost:5173', 'http://localhost:8081' ];
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
